b390bd77ec65c20578f545b7e883099cf6acb492
Juice Wrld <87148031+969ali969@users.noreply.github.com>
2021-12-21 20:01:29 +0330

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+I have done this just to get more familiar with pyautogui library
+.
+.
+.
+Go to https://www.agame.com/game/magic-piano-tiles and maximize ur browser (just ur browser)
+You should have 1920x1080 resolution or it wont work!
+.
+.
+.
+While u have ur browser maximized run the piano.py and then start the game.
---
+++ paino.py
@@ -0,0 +1,38 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+#tile 1 position = X: 516 Y:  350 RGB: (  0,  34,  64)
+#tile 2 position = X: 619 Y:  350 RGB: (  0,  34,  64)
+#tile 3 position = X: 732 Y:  350 RGB: (  0,  34,  64)
+#tile 4 position = X: 835 Y:  350 RGB: (  0,  34,  64)
+
+time.sleep(5)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.09)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+
+def clicked():
+    print("Click!")
+
+
+while keyboard.is_pressed("s") == False:
+    if pyautogui.pixel(516, 400) [0] == 0:
+            click(516,400)
+            clicked()
+    if pyautogui.pixel(619, 400) [0] == 0:
+            click(619,400)
+            clicked()
+    if pyautogui.pixel(732, 400) [0] == 0:
+            click(732,400)
+            clicked()
+    if pyautogui.pixel(835, 400) [0] == 0:
+            click(835,400)
+            clicked()
